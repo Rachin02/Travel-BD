91799af74ae6e6a621629cd2f76a4dd14605bf90
Lutfur Rahman Tanvir <tanvir15-5649@diu.edu.bd>
2025-07-31 18:47:10 +0600

    destination, hire_guide, hotel_booking added with some frontend fixes

--- Backend/travel_bd/settings.py
+++ Backend/travel_bd/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
+    'destination',
+    'hire_guide',
+    'hotel_booking',
 ]
 
 MIDDLEWARE = [
